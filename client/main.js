09df6f5f1e06bfaf20175f762804c340b0dc097d
Nathan Thomas <nathan.gordon.thomas@gmail.com>
2020-04-16 23:24:27 -0600

    Update imports

--- client/main.js
+++ client/main.js
@@ -1,7 +1,11 @@
+import { Meteor } from 'meteor/meteor'
 import { Template } from 'meteor/templating';
-// import { ReactiveVar } from 'meteor/reactive-var';
+import { Recipes } from '/collections/Recipes.js';
+import { ReactiveVar } from 'meteor/reactive-var';
 //
 import './main.html';
-import '../lib/routes.js'
+import '../lib/routes.js';
 import './layouts/HomeLayout.html';
 import './layouts/MainLayout.html';
+import './recipes/Recipes.html';
+import './recipes/NewRecipe.html';
