6e4964f8dff1c8084435f99f7eb62f031b9b3196
Nathan Thomas <nathan.gordon.thomas@gmail.com>
2020-04-18 18:48:59 -0600

    Allow recipie instances to be added to db

--- collections/Recipes.js
+++ collections/Recipes.js
@@ -1,5 +1,12 @@
 Recipes = new Meteor.Collection('recipes');
 
+Recipes.allow({
+  insert: function(userId, doc) {
+    return !!userId;
+    // if user id exists you can add a recipe
+  }
+});
+
 
 RecipeSchema = new SimpleSchema({
   name: {
