bef42d66a22056a768fa0d18e36aefea9d624aea
Nathan Thomas <nathan.gordon.thomas@gmail.com>
2020-04-26 23:07:30 -0600

    Add template level subscription

--- client/recipes/Recipes.js
+++ client/recipes/Recipes.js
@@ -1,5 +1,10 @@
-Meteor.subscribe('recipes');
-// Meteor.subscribe(Recipes);
+Template.Recipes.onCreated(function(){
+  var self = this;
+  self.autorun(function(){
+// ^^ unscribes us from any old subscriptions
+    self.subscribe('recipes');
+  })
+});
 
 Template.Recipes.helpers({
   recipes: ()=> {
